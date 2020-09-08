1b066a01dae8e09b12e49fca30ffd1f86838e088
nakaryo19 <ryoryoest19@gmail.com>
2020-09-08 16:47:19 +0900

    tagsの削除

--- db/migrate/20200907071545_create_tags.rb
+++
@@ -1,8 +0,0 @@
-class CreateTags < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tags do |t|
-      t.references :user,       null: false, foreign_key:true
-      t.timestamps
-    end
-  end
-end
