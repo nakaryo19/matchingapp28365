58b4d8978c87b0a56eddedd0501aff35151d56a3
nakaryo19 <ryoryoest19@gmail.com>
2020-09-09 16:56:33 +0900

    READMEの追記5

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   root to: "toppages#index"
-  devise_for :users, :controllers => {
-    :sessions      => "users/sessions",
-    :registrations => "users/registrations",
-    :passwords     => "users/passwords",
-  }
-  resources :users, :only => [:index, :show]
+  devise_for :users   
+    resources :users, :only => [:index, :show]
   
-  resources :tags do
-    collection do
-      get :search
-    end
-  end
 end
